473b6da5033bccee6d8874c0727f037669fe451f
mehdikhfifi <145874140+mehdikhfifi@users.noreply.github.com>
2024-11-17 21:29:22 -0800

    Time: 0 ms (100%), Space: 7.7 MB (74.93%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -6,16 +6,15 @@ public:
     int uniquePaths(int m, int n) {
         vector<int> row(n, 1);
 
-        for (int i = 0; i < m-1; i++){
+        for (int i = 0; i < m - 1; i++) {
 
             vector<int> new_row(n, 1);
 
-            for (int j = n-2; j >= 0; j--){
-                new_row[j] = new_row[j+1] + row[j];
+            for (int j = n - 2; j >= 0; j--) {
+                new_row[j] = new_row[j + 1] + row[j];
             };
             row = new_row;
         };
         return row[0];
-
     }
 };
