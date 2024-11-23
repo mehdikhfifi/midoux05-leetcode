e95fb28dea038701ce1148622de122f3f4ffaa3e
mehdikhfifi <145874140+mehdikhfifi@users.noreply.github.com>
2024-11-23 09:59:12 -0800

    Time: 198 ms (39.58%), Space: 56.3 MB (85.37%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.cpp
+++ 1972-rotating-the-box/1972-rotating-the-box.cpp
@@ -2,28 +2,23 @@
 #include <vector>
 using namespace std;
 
-
-
-
 class Solution {
 public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
-        
-        for (auto& row : box){
+
+        for (auto& row : box) {
 
             int dropPos = row.size() - 1;
 
-            for (int currPos = dropPos; currPos >-1; currPos--){
+            for (int currPos = dropPos; currPos > -1; currPos--) {
 
-                if (row[currPos] == '*'){
-                     dropPos = currPos - 1;
+                if (row[currPos] == '*') {
+                    dropPos = currPos - 1;
 
-                }
-                else if (row[currPos] == '#'){
+                } else if (row[currPos] == '#') {
                     swap(row[dropPos], row[currPos]);
-                    dropPos --;
+                    dropPos--;
                 }
-               
             }
         }
 
@@ -32,16 +27,14 @@ public:
 
         vector<vector<char>> rotatedBox(cols, vector<char>(rows));
 
-        for (int i = 0; i < rows ; i++){
+        for (int i = 0; i < rows; i++) {
 
-        for (int j = 0; j < cols ; j++){
-        
-        rotatedBox[j][rows - 1 - i] = box[i][j];
-        }
+            for (int j = 0; j < cols; j++) {
+
+                rotatedBox[j][rows - 1 - i] = box[i][j];
+            }
         }
 
         return rotatedBox;
-
-        
     }
 };
