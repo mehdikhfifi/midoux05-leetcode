bcc11ebd2f69a51cdfa092e7528b816bcf402538
mehdikhfifi <145874140+mehdikhfifi@users.noreply.github.com>
2024-11-23 20:35:37 -0800

    Time: 744 ms (76.01%), Space: 17.1 MB (52.96%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -8,8 +8,9 @@ class Solution:
 
 
         for coin in coins:
-            for i in range(coin, amount + 1):
+            for i in range(coin, amount+1):
                 dp[i] = min(dp[i], dp[i-coin] + 1)
-        
+
+
 
         return dp[amount] if dp[amount] != float('inf') else -1
