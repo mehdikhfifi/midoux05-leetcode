db81bce19fc5d91c9ff6890742a8ca9f01783e12
mehdikhfifi <145874140+mehdikhfifi@users.noreply.github.com>
2024-11-25 10:41:41 -0800

    Time: 7 ms (71.16%), Space: 16.7 MB (52.79%) - LeetHub

--- 0787-sliding-puzzle/0787-sliding-puzzle.py
+++ 0787-sliding-puzzle/0787-sliding-puzzle.py
@@ -1,41 +1,43 @@
-from collections import defaultdict
+from collections import deque
 
 class Solution:
     def slidingPuzzle(self, board: List[List[int]]) -> int:
         
 
-        start = ''.join(str(num) for row in board for num in row)
+
+        start = ''.join(str(nums) for row in board for nums in row)
 
         target = '123450'
-
-
-        neighbours = {
+        neighbors = {
             0: [1, 3], 1: [0, 2, 4], 2: [1, 5],
             3: [0, 4], 4: [1, 3, 5], 5: [2, 4]
         }
 
-        queue = deque([(start, 0)]) 
+
+
+        queue = deque([(start, 0)])
         visited = set([start])
 
+
         while queue:
-            current, moves = queue.popleft()
 
-
-            if current == target:
+            state, moves = queue.popleft()
+            if state == target:
                 return moves
-            zero_idx= current.index('0')
 
-            for neighbor in neighbours[zero_idx]:
-                new_state = list(current)
+            
+            zero_idx = state.index('0')
+            
+            for neighbor in neighbors[zero_idx]:
+                new_state = list(state)
                 new_state[zero_idx], new_state[neighbor] = new_state[neighbor], new_state[zero_idx]
                 new_state = ''.join(new_state)
 
-
                 if new_state not in visited:
                     visited.add(new_state)
-                    queue.append((new_state, moves+1))
+                    queue.append((new_state, moves +1))
         
         return -1
 
 
-
+            
