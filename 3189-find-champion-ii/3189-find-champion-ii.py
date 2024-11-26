1f0b602b7b5da2cac9ab7b3accded0e81372458f
mehdikhfifi <145874140+mehdikhfifi@users.noreply.github.com>
2024-11-25 20:11:21 -0800

    Time: 14 ms (37.75%), Space: 17.7 MB (58.3%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.py
+++ 3189-find-champion-ii/3189-find-champion-ii.py
@@ -1,23 +1,24 @@
-from typing import List
 from collections import defaultdict
 
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
+
         if n == 0:
-            return -1  # No teams present
-
-        # Initialize in-degree for all teams to 0
-        in_degree = [0] * n
-
-        # Compute in-degree for each team
-        for u, v in edges:
-            in_degree[v] += 1
-
-        # Identify all teams with zero in-degree
-        champions = [i for i in range(n) if in_degree[i] == 0]
-
-        # Determine if there's a unique champion
-        if len(champions) == 1:
-            return champions[0]
-        else:
             return -1
+
+        
+
+
+        hashmap = [0] * (n)
+
+        for x,y in edges:
+            hashmap[y] +=1
+
+        
+        m = [x for x in range(n) if hashmap[x] == 0 ]
+
+        if len(m) == 1 :
+            return m[0]
+        return -1
+        
+        
