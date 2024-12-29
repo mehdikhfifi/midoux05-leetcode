b84e838ea213513ca5527b16dd536bbd28099232
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2024-12-29 14:07:38 +0100

    Time: 3190 ms (23.21%), Space: 18.1 MB (30.4%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        
+
+        least = n
+
+        dp = list(range(n+1))
+
+        for i in range(1,n+1):
+
+            for j in range(i):
+
+                if i - j**2 >= 0:
+                    dp[i] = min(dp[i], dp[i-j*j] + 1)
+                else:
+                    break
+
+        return dp[-1]
+        
+
+        
