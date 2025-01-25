64eda651cdc868e123d525cacaf81800608e6250
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-01-25 15:34:09 -0800

    Time: 10589 ms (5.24%), Space: 29.4 MB (20.09%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -4,12 +4,37 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums = [1] + nums + [1]
-        n = len(nums)
-        dp = [[0 for _ in range(n)] for _ in range(n)]
-
-        for right in range(2, n):
-            for left in range(right - 2, -1, -1):
-                dp[left][right] = max([dp[left][m] + dp[m][right] + nums[left] * nums[m] * nums[right] for m in range(left + 1, right)])
         
-        return dp[0][n - 1]
+
+
+
+# 3,1,5,8
+# the subrpoblems are derived by first looking at which number is last popped
+
+        nums = [1] + nums + [1]
+
+        dp = {}
+        
+        # this dp hashmap is for memoization
+
+
+        def dfs(l, r):
+            # this function finds the maximum coins you can collect between range l and r
+
+            # first handle the base cases
+            if l > r:
+                return 0 # shit don't make sense
+            if (l,r) in dp:
+                return dp[(l,r)]
+
+            # otherwise brute force and find the optimal number of coins recursively
+            dp[(l,r)] = 0
+            for i in range(l, r+1):
+                # finds which index in l to r should be removed last, 
+                # the one that leads to the most optimal maximum number of coins
+                coins = nums[i] * nums[r+1] * nums[l-1] # if i is popped last, this is the last operation
+                coins += dfs(l, i-1) + dfs(i+1, r)
+                dp[(l,r)] = max(coins, dp[(l,r)])
+            return dp[(l,r)]
+        return dfs(1,len(nums) -2)
+
