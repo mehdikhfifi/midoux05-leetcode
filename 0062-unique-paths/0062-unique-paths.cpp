b6f453508b91fd89ac72d5daa6efe4c5332c616d
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-01-25 11:15:38 -0800

    Time: 0 ms (100%), Space: 9.4 MB (33.05%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,31 @@
+
 #include <iostream>
 #include <vector>
+
 using namespace std;
+
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> row(n, 1);
+        // m is the rows
+        // n is the columns
 
-        for (int i = 0; i < m - 1; i++) {
+        // first populate the grid with 1s in the sides
+        using vec2d = vector<vector<int>>;
 
-            vector<int> new_row(n, 1);
-
-            for (int j = n - 2; j >= 0; j--) {
-                new_row[j] = new_row[j + 1] + row[j];
-            };
-            row = new_row;
-        };
-        return row[0];
+        vec2d matrix(m, vector<int>(n,0));
+        
+        for (int i = 0; i < m ; i++){
+            matrix[i][0]  = 1;
+        }
+        for (int j = 0; j < n ; j++){
+            matrix[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n ; j++){
+                matrix[i][j] += matrix[i-1][j] + matrix[i][j-1];
+            }
+        }
+        return matrix[m-1][n-1];
     }
 };
