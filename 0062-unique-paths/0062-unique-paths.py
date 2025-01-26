c18ae339470ae56970a81f3457977831e8f09324
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-01-25 21:32:49 -0800

    Time: 0 ms (100%), Space: 17.8 MB (45.24%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,16 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+
+        matrix = [[1]* n for _ in range(m)]
+
+        # for i in range(m):
+        #     matrix[i][0] = 1
+
+        # for j in range(n):
+        #     matrix[0][j] = 1
+        
+        for i in range(1,m):
+            for j in range(1,n):
+                matrix[i][j] = matrix[i-1][j] + matrix[i][j-1]
+        return matrix[-1][-1]
