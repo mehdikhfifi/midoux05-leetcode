2d15df5c8990f257c8dd92e8124ecf10e4b4ee64
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-01-27 09:06:11 -0800

    Time: 56 ms (30.35%), Space: 38.1 MB (28.33%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        
+
+
+        hashmap = {0:-1}
+        total = 0
+
+        
+        for i in range(len(nums)):
+            total += nums[i]
+            remainder = total % k
+
+            if remainder not in hashmap:
+                hashmap[remainder] = i
+            elif i - hashmap[remainder]  > 1:
+                return True
+        
+        return False
