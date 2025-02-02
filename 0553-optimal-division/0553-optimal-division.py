bc9f81a1adafd4b5827c8a2378ec0b07c9d15fa5
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-02-02 11:35:09 -0800

    Time: 4 ms (9.8%), Space: 17.9 MB (13.24%) - LeetHub

--- 0553-optimal-division/0553-optimal-division.py
+++ 0553-optimal-division/0553-optimal-division.py
@@ -1,18 +1,51 @@
+from typing import List
+
 class Solution:
     def optimalDivision(self, nums: List[int]) -> str:
-
-        if len(nums) == 1: 
+        n = len(nums)
+        if n == 1:
             return str(nums[0])
-        if len(nums) == 2:
-            return str(nums[0]) + "/" + str(nums[1]) 
+        if n == 2:
+            return f"{nums[0]}/{nums[1]}"
+        
+        # Initialize DP tables
+        dp = [[0] * n for _ in range(n)]   # Stores max values
+        min_dp = [[0] * n for _ in range(n)]  # Stores min values
+        exp = [[""] * n for _ in range(n)]   # Stores max expressions
+        min_exp = [[""] * n for _ in range(n)]  # Stores min expressions
+        
+        # Base cases
+        for i in range(n):
+            dp[i][i] = min_dp[i][i] = nums[i]
+            exp[i][i] = min_exp[i][i] = str(nums[i])
+        
+        # DP computation for subarrays of length > 1
+        for length in range(2, n + 1):  # Length of subarray
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i][j] = float('-inf')
+                min_dp[i][j] = float('inf')
+                
+                # Try all possible splits
+                for k in range(i, j):
+                    # Maximize dp[i][j]
+                    value = dp[i][k] / min_dp[k+1][j]
+                    if value > dp[i][j]:
+                        dp[i][j] = value
+                        # If k+1 to j contains more than one element, add parentheses
+                        if k + 1 == j:
+                            exp[i][j] = f"{exp[i][k]}/{min_exp[k+1][j]}"
+                        else:
+                            exp[i][j] = f"{exp[i][k]}/({min_exp[k+1][j]})"
+                    
+                    # Minimize min_dp[i][j]
+                    value = min_dp[i][k] / dp[k+1][j]
+                    if value < min_dp[i][j]:
+                        min_dp[i][j] = value
+                        # If k+1 to j contains more than one element, add parentheses
+                        if k + 1 == j:
+                            min_exp[i][j] = f"{min_exp[i][k]}/{exp[k+1][j]}"
+                        else:
+                            min_exp[i][j] = f"{min_exp[i][k]}/({exp[k+1][j]})"
 
-
-        my_string = str(nums[0]) + "/(" + str(nums[1])
-
-        for x in nums[2:]:
-            
-            my_string += "/" + str(x)
-
-        my_string += ")"
-
-        return my_string
+        return exp[0][n-1]
