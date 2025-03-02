5a10e4f63b58690f9172ec3f71b850064d688080
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-03-01 17:39:00 -0800

    Time: 71 ms (73.36%), Space: 172.9 MB (85.48%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,99 +1,104 @@
-
 #include <iostream>
 #include <unordered_map>
 
 using namespace std;
-
 class Node {
+
 public:
-    int key;
-    int value;
-    Node* prev;
-    Node* next;
-    Node(int key, int value) {
-        this->key = key;
-        this->value = value;
-        this->prev = nullptr;
-        this->next = nullptr;
-    }
+  int key;
+  int value;
+  Node *prev;
+  Node *next;
+
+  Node(int key, int value) {
+    this->key = key;
+    this->value = value;
+    this->prev = nullptr;
+    this->next = nullptr;
+  }
 };
 
 class LRUCache {
 
 private:
-    int capacity;
-    unordered_map<int, Node*> cache;
-    Node* head;
-    Node* tail;
+  int capacity;
+  unordered_map<int, Node *> cache;
+  Node *head;
+  Node *tail;
 
 public:
-    LRUCache(int capacity) {
-        this->capacity = capacity;
-        cache.reserve(capacity);
-        head = new Node(0, 0);
-        tail = new Node(0, 0);
-        head->next = tail;
-        tail->prev = head;
+  LRUCache(int capacity) {
+    this->capacity = capacity;
+    cache.reserve(capacity);
+    head = new Node(0, 0);
+    tail = new Node(0, 0);
+    head->next = tail;
+    tail->prev = head;
+  }
+
+  int get(int key) {
+
+    // check if the value exists first:
+
+    if (cache.find(key) == cache.end()){
+        return -1;
+    } 
+        Node* resnode = cache[key];
+        remove(resnode);
+        insert(resnode);
+        return resnode->value;
+        
+    
+
+
+
+  }
+
+  void put(int key, int value) {
+
+    // check first if key already exists:
+    if (cache.find(key) != cache.end()) {
+
+      Node *temp = cache[key];
+      temp->value = value;
+      remove(temp);
+      insert(temp);
+
+
     }
+    // check capacity:
+    else {
+      if (cache.size() == this->capacity) {
+        // last item is
+        Node *last_node = tail->prev; // this is the last node
+        cache.erase(last_node->key);
+        remove(last_node);
+      }
+      Node* new_node = new Node(key,value);
+      cache[key] = new_node;
+      insert(new_node);
 
-    int get(int key) {
-
-        if (cache.find(key) == cache.end()) {
-            return -1;
-        }
-
-        Node* temp = cache[key];
-
-        remove(temp);
-        insert(temp);
-
-        return temp->value;
     }
+    //
+  }
 
-    void put(int key, int value) {
-
-        // check if it exists already
-
-        if (cache.find(key) != cache.end()) {
-
-            Node* node = cache[key];
-            node->value = value;
-            remove(node);
-            insert(node);
-
-        } else {
-            if (cache.size() == capacity) {
-                Node* lru = tail->prev;
-
-                cache.erase(lru->key);
-                remove(lru);
-                delete lru;
-            }
-        // youcan now insert it
-        Node* newnode = new Node(key, value);
-        cache[key] = newnode; // ??????? wtf is going on
-        insert(newnode);
-        }
-    }
-
-    // helper functions
 private:
-    void remove(Node* node) {
-        node->prev->next = node->next;
-        node->next->prev = node->prev;
-    }
+  void remove(Node* node) {
 
-    void insert(Node* node) {
-        node->next = head->next;
-        node->prev = head;
-        head->next->prev = node;
-        head->next = node;
-    }
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+
+
+  }
+
+  void insert(Node* node) {
+
+      node->next = head->next;
+      node->prev = head;
+      head->next->prev = node;
+    head->next = node;
+
+
+
+  }
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
