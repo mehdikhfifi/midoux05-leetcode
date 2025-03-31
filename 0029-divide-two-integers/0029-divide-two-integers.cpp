bebe1e4e1ebc0ebfcc0f454f0805ae650a1508f6
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-03-30 23:34:53 -0700

    Time: 0 ms (100%), Space: 8.6 MB (71.85%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,39 +1,46 @@
-#include <iostream>
-#include <cmath>
-
-
 class Solution {
-    public:
-        int divide(int x, int y) {
-
-            if (y ==0){
-                throw std::overflow_error("Divisiong by zero");
-            }
-
-            if (x == INT_MIN && y== -1){
-                return INT_MAX;
-            }
-            int sign = (x>0) ^(y > 0) ? -1 : 1;
-
-            long long dividend = abs( (long long ) x);
-            long long divisor = abs( (long long ) y);
+public:
+    int divide(int x, int y) {
 
 
-            long long  quotient = 0;
-            
-            while (divisor <= dividend){
-                long long temp = divisor;
-                
-                long long multiple =1;
+        // check for runtime errors
 
-                while (dividend >= (temp << 1) ){
-                    temp <<= 1;
-                    multiple <<=1;
-
-                }       
-                dividend -= temp;
-                quotient += multiple;
+        if(y ==0){
+            throw std::overflow_error("Division by zero");
         }
-        return quotient * sign ;
+
+        if (x == INT_MIN && y == -1){
+            return INT_MAX;
+        }
+
+
+        long long dividend = abs( (long long )x);
+        long long divisor = abs( (long long ) y);
+        long long quotient =0 ;
+
+        long long sign = (x >0) ^ (y> 0) ? -1: 1;
+        
+
+        while (dividend >= divisor){
+            
+
+            long long temp = divisor;
+            long long multiple = 1;
+
+            while (dividend >= (temp << 1)){
+
+                temp <<= 1;
+                multiple <<=1;
+            }
+            dividend -= temp;
+            quotient += multiple;
+        }
+        cout << quotient << endl;
+        cout << sign << endl;
+
+        return quotient * sign;
+
+
+
     }
 };
