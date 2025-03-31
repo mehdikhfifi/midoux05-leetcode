404abe99963d78a948a0c8bab4bfd390cc075ad4
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-03-30 23:27:25 -0700

    Time: 0 ms (100%), Space: 8.6 MB (38.51%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+
+class Solution {
+    public:
+        int divide(int x, int y) {
+
+            if (y ==0){
+                throw std::overflow_error("Divisiong by zero");
+            }
+
+            if (x == INT_MIN && y== -1){
+                return INT_MAX;
+            }
+            int sign = (x>0) ^(y > 0) ? -1 : 1;
+
+            long long dividend = abs( (long long ) x);
+            long long divisor = abs( (long long ) y);
+
+
+            long long  quotient = 0;
+            
+            while (divisor <= dividend){
+                long long temp = divisor;
+                
+                long long multiple =1;
+
+                while (dividend >= (temp << 1) ){
+                    temp <<= 1;
+                    multiple <<=1;
+
+                }       
+                dividend -= temp;
+                quotient += multiple;
+        }
+        return quotient * sign ;
+    }
+};
