586668324612e541417418ed13039cc0cefc803d
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-04-01 13:44:09 -0700

    Time: 0 ms (100%), Space: 8.5 MB (96.86%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,57 +1,64 @@
 #include <iostream>
-#include <cmath>
+
+
 
 
 using namespace std;
 
 
+
+
+
+
 class Solution {
-    public:
-        int divide(int x, int y) {
+    public: 
+
+    int divide(int x , int y){
 
 
 
-
-
-            if (y == 0){
-                throw std::overflow_error("division by zero");
-            }
-
-            if (x == INT_MIN && y == -1){
-                return INT_MAX;
-            }
-
-
-            long long dividend = abs((long long )x);
-            long long divisor = abs((long long ) y);
-
-            int sign = (x > 0) ^ (y>0) ?  -1 : 1; // if they're the same sign will be 1 else -1
-
-
-
-
-            long long quotient = 0;
-
-
-            while (dividend >= divisor){
-
-
-                long long  multiple = 1;
-                long long temp = divisor;
-
-                
-
-                while (dividend >= (temp << 1)){
-
-                    temp <<=1;
-                    multiple <<=1;
-                }
-
-                quotient+= multiple;
-
-                dividend -= temp;
-            }
-            
-            return quotient * sign;
+        if (x == INT_MIN && y == -1){
+            return INT_MAX;
         }
-    };
+        
+        if (y == 0){
+            std:throw overflow_error("Cannot divide by zero");
+        }
+
+
+
+
+        long long dividend = abs( (long long )x);
+        long long divisor = abs( (long long ) y);
+
+
+        int sign = (x > 0) ^ (y > 0) ? -1 : 1;
+
+        long long quotient = 0;
+
+
+        while (dividend >= divisor){
+
+
+            long long multiple =1;
+            long long temp = divisor;
+
+            while (dividend >= (temp << 1)){
+
+                multiple <<=1;
+
+                temp <<=1;
+
+
+            }
+            dividend -= temp;
+            quotient += multiple;
+        }
+
+
+        return quotient * sign;
+
+
+
+    }
+};
