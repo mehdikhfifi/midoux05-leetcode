d3070dd97cb58d0706a66e9d60dbb13f30a140ee
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-04-07 10:13:12 -0700

    Time: 15 ms (87.65%), Space: 13.4 MB (70.07%) - LeetHub

--- 2444-longest-ideal-subsequence/2444-longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/2444-longest-ideal-subsequence.cpp
@@ -18,9 +18,14 @@ public:
 
             int max_prev = 0;
 
+            // loop goes left to right and only extends subsequences using characters that have already been processed
+            // what this looks like is that the dp[i] for characters that are ahead of c will be zero so it won't affect our max_prev
+
             for (int i = max(0,idx-k); i <= min(25, idx + k) ; i++){
                 max_prev = max(max_prev, dp[i]);
             }
+            // this for loop checks all the characters we can reasonably jump from in our sequence
+
 
             dp[idx] = max(dp[idx], max_prev + 1);
         }
