ba026424d3b439792f518026e1c93021aff0c811
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-04-08 15:40:02 -0700

    Time: 22 ms (70.3%), Space: 13.3 MB (70.3%) - LeetHub

--- 2444-longest-ideal-subsequence/2444-longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/2444-longest-ideal-subsequence.cpp
@@ -1,32 +1,32 @@
-#include <vector>
-#include <iostream>
 #include <string>
+#include <iostream>
+#include <vector>
+
 
 using namespace std;
-
 class Solution {
-public:
-    int longestIdealString(string s, int k) {
-        
+    public:
+        int longestIdealString(string s, int k) {
 
 
-        vector<int> dp(26,0);
+            vector<int> dp(26,0);
 
 
-        for (char c: s){
+            for (char c : s){
+                int index = c - 'a';
 
-            int index = c - 'a';
+                int max_prev = 0;
 
-            int max_prev = 0;
+                for (int i = max(0,index-k); i <= min(25,index + k); i++){
+                    max_prev = max(max_prev , dp[i]);
+                }
 
-            for (int i = max(0,index - k); i <= min(25,index + k); i++){
-                
-                max_prev = max(max_prev, dp[i]);
+                dp[index] = max(dp[index],max_prev + 1 );
+
             }
 
-            dp[index] = max(dp[index], max_prev +1) ;
+            return *max_element(dp.begin(), dp.end());
 
+            
         }
-    return *max_element(dp.begin(), dp.end());
-    }
-};
+    };
