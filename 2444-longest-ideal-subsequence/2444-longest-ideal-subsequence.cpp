4a8183553784d70f6c6f6b1b4c5140b6472dfee4
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-04-08 00:26:04 -0700

    Time: 19 ms (78.42%), Space: 13.4 MB (56.83%) - LeetHub

--- 2444-longest-ideal-subsequence/2444-longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/2444-longest-ideal-subsequence.cpp
@@ -1,34 +1,32 @@
 #include <vector>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Solution {
 public:
     int longestIdealString(string s, int k) {
+        
 
-        
-        
 
         vector<int> dp(26,0);
 
 
-        for (char c : s){
-            int idx = c - 'a';
+        for (char c: s){
+
+            int index = c - 'a';
 
             int max_prev = 0;
 
-            // loop goes left to right and only extends subsequences using characters that have already been processed
-            // what this looks like is that the dp[i] for characters that are ahead of c will be zero so it won't affect our max_prev
-
-            for (int i = max(0,idx-k); i <= min(25, idx + k) ; i++){
+            for (int i = max(0,index - k); i <= min(25,index + k); i++){
+                
                 max_prev = max(max_prev, dp[i]);
             }
-            // this for loop checks all the characters we can reasonably jump from in our sequence
 
+            dp[index] = max(dp[index], max_prev +1) ;
 
-            dp[idx] = max(dp[idx], max_prev + 1);
         }
-        return *max_element(dp.begin(), dp.end());
+    return *max_element(dp.begin(), dp.end());
     }
 };
