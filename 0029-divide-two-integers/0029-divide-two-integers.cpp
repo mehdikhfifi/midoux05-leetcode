4bc9d6fce7a0c08459f644bd5be00e3d387b6f63
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-04-08 21:50:19 -0700

    Time: 0 ms (100%), Space: 8.7 MB (38.81%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,64 +1,46 @@
 #include <iostream>
-
-
+#include <string>
+#include <vector>
 
 
 using namespace std;
 
 
-
-
-
-
 class Solution {
-    public: 
-
-    int divide(int x , int y){
-
-
-
+public:
+    int divide(int x, int y) {
+        
         if (x == INT_MIN && y == -1){
             return INT_MAX;
         }
-        
         if (y == 0){
-            std:throw overflow_error("Cannot divide by zero");
+            throw runtime_error("cannot divide by zero");
         }
 
+        int sign =  (x > 0) ^ (y > 0)  ? -1 : 1;
 
-
-
-        long long dividend = abs( (long long )x);
-        long long divisor = abs( (long long ) y);
-
-
-        int sign = (x > 0) ^ (y > 0) ? -1 : 1;
+        long long dividend = abs((long long) x);
+        long long divisor = abs((long long) y);
 
         long long quotient = 0;
 
 
         while (dividend >= divisor){
 
-
-            long long multiple =1;
             long long temp = divisor;
+            long long multiple = 1;
 
             while (dividend >= (temp << 1)){
 
                 multiple <<=1;
-
                 temp <<=1;
-
-
             }
+
             dividend -= temp;
             quotient += multiple;
+
         }
 
-
         return quotient * sign;
-
-
-
     }
 };
