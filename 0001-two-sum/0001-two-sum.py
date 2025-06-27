5ff753ac59ecfdbfd347a97656712c6fc748d259
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-06-26 18:32:18 -0700

    Time: 0 ms (100%), Space: 18.9 MB (50.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,19 +1,17 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        answer = []
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i != j and nums[i] + nums[j] == target:
-                    if i not in answer and j not in answer:
-                        answer.append(i)
-                        answer.append(j)
-        
-        return answer
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
 
 
+
+        hastable = {}
+
+        for index, nums in enumerate(nums):
+
+
+            
+            if target - nums in hastable:
+                return list(sorted([hastable[target-nums], index]))
+            else:
+                hastable[nums] = index
         
+
