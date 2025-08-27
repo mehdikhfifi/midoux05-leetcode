bb0556af66ac527e00273c5d60ffdefe8a04d818
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-08-27 14:32:10 -0700

    Time: 447 ms (55.89%), Space: 65.9 MB (32.03%) - LeetHub

--- 2175-count-nodes-with-the-highest-score/2175-count-nodes-with-the-highest-score.py
+++ 2175-count-nodes-with-the-highest-score/2175-count-nodes-with-the-highest-score.py
@@ -1,31 +1,24 @@
 class Solution:
     def countHighestScoreNodes(self, parents: List[int]) -> int:
         
+
         graph = collections.defaultdict(list)
         n = len(parents)
-
         for child, parent in enumerate(parents):
             graph[parent].append(child)
-
-        freq = collections.Counter()
+        freq = Counter()
         def count_nodes(node):
-            # returns size of node tree, updates freq with its score
-            p,s = 1,0
+            s,p = 0,1
+            
             for child in graph[node]:
-                count_child = count_nodes(child)
-                s += count_child
-                p *= count_child
-            p *= max(1, (n-1-s)) # everything above the node
-            freq[p] +=1
-            return s + 1
+                res = count_nodes(child)
+                s+= res
+                p*= res
+            p *= max(1, (n-1-s))
+            freq[p] += 1
+            return s +1
+
 
 
         count_nodes(0)
-        return freq[max(freq.keys())] 
-
-
-        
-
-
-
-        
+        return freq[max(freq.keys())]
