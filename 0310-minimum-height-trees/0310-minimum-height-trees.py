e173e5a9a02e63466951e061a1dbb967a91057eb
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-08-28 22:04:40 -0400

    Time: 54 ms (51.38%), Space: 25.8 MB (75.58%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,37 +1,38 @@
 from collections import defaultdict
-
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        if n <=2:
-            return list(range(n))
+        if n == 1:
+            return [0]
+        graph = defaultdict(list)
 
-        graph = {i : [] for i in range(n)}
-
+        degree = [0] * n
         for x,y in edges:
             graph[x].append(y)
             graph[y].append(x)
-        
+            degree[x] +=1
+            degree[y] +=1
+        for node in graph.keys():
+            degree[node] = len(graph[node])
+        leaves = deque([i for i in range(n) if degree[i] == 1])
 
-        
-        leaves = [i for i in graph if len(graph[i]) == 1]
+        remaining_nodes = n
 
-        remaining = n
+        while remaining_nodes >2:
+            size = len(leaves)
+            remaining_nodes -= size
+            for _ in range(size):
+                leaf = leaves.popleft()
+                degree[leaf] = 0
+                for parent in graph[leaf]:
+                    degree[parent] -=1
+                    if degree[parent] ==1:
+                        leaves.append(parent)
+        return list(leaves)
 
-        while remaining > 2:
-
-            remaining -= len(leaves)
-            new_leaves = []
-            for leaf in leaves:
-                neighbor = graph[leaf].pop()
-                graph[neighbor].remove(leaf)
-
-                if len(graph[neighbor]) ==1:
-                    new_leaves.append(neighbor)
-            leaves= new_leaves
             
-        
-        return leaves
-
 
         
+        
 
+
+        
