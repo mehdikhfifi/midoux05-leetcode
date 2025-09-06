5473119a0a3bc42552e7e1e32e6f83d2492be0c4
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-09-06 11:27:49 -0400

    Time: 20 ms (96.1%), Space: 20.5 MB (65.03%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.py
+++ 1558-course-schedule-iv/1558-course-schedule-iv.py
@@ -1,15 +1,27 @@
-from functools import lru_cache
-from collections import defaultdict
-
 class Solution:
-    def checkIfPrerequisite(self, numCourses, prerequisites, queries):
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        
+
+
         graph = defaultdict(list)
-        for u, v in prerequisites:
-            graph[u].append(v)
+        indeg = [0] * numCourses
+        for prereq, course in prerequisites:
+            graph[prereq].append(course)
+            indeg[course] +=1
+        
+        prereq_sets = [set() for _ in range(numCourses)]
+        q = deque([i for i in range(numCourses) if indeg[i] ==0])
 
-        @lru_cache(None)
-        def explore(u, v):
-            if u == v: return True
-            return any(explore(child, v) for child in graph[u])
+        while q:
+            u = q.popleft()
+            for v in graph[u]:
+                prereq_sets[v].add(u)
+                prereq_sets[v] |= prereq_sets[u] # adding all prereqs of u as they are obvs prereqs of v as well
+                indeg[v] -=1
+                if indeg[v] ==0:
+                    q.append(v)
+        
+        return [(x in prereq_sets[y]) for x,y in queries]
+        
+        
 
-        return [explore(x, y) for x, y in queries]
