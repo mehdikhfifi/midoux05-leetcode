b94c826302bec47f75815ced64b1911a5cd72ef7
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-11-02 16:44:24 -0800

    Time: 117 ms (55.37%), Space: 26.3 MB (77.45%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.py
@@ -2,22 +2,33 @@ class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
 
 
-        i = 0
-        needed_time = 0
-        while i < len(colors):
+        # i = 0
+        # needed_time = 0
+        # while i < len(colors):
 
-            chosen_color = colors[i]
-            chosen_time = neededTime[i]
-            k = i +1
-            res = [chosen_time]
-            while k < len(colors) and colors[k] == chosen_color:
-                res.append(neededTime[k])
-                k+=1
-            sum_res = sum(res)
-            max_res = max(res)
-            needed_time += sum(res) - max(res)
-            i = k
-        return needed_time
+        #     chosen_color = colors[i]
+        #     chosen_time = neededTime[i]
+        #     k = i +1
+        #     res = [chosen_time]
+        #     while k < len(colors) and colors[k] == chosen_color:
+        #         res.append(neededTime[k])
+        #         k+=1
+        #     sum_res = sum(res)
+        #     max_res = max(res)
+        #     needed_time += sum(res) - max(res)
+        #     i = k
+        # return needed_time
+
+        res = neededTime[0]
+        max_i = neededTime[0]
+
+        for i in range(1,len(colors)):
+            if colors[i-1] != colors[i]:
+                res -= max_i
+                max_i = 0
+            res += neededTime[i]
+            max_i = max(max_i, neededTime[i])
+        return res - max_i
 
 
         
