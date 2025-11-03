8158a41ae857817c964ad9d25cd04b826d8e5f6e
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-11-02 16:38:25 -0800

    Time: 185 ms (12.27%), Space: 26.5 MB (18.81%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.py
@@ -1,24 +1,23 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
+
+
+        i = 0
+        needed_time = 0
+        while i < len(colors):
+
+            chosen_color = colors[i]
+            chosen_time = neededTime[i]
+            k = i +1
+            res = [chosen_time]
+            while k < len(colors) and colors[k] == chosen_color:
+                res.append(neededTime[k])
+                k+=1
+            sum_res = sum(res)
+            max_res = max(res)
+            needed_time += sum(res) - max(res)
+            i = k
+        return needed_time
+
+
         
-
-        # colorful when there are no adjacently similar balloon
-        n = len(neededTime)
-
-        dp = n*[0]
-        dp[0] = 0
-        
-
-        for i in range(1,n):
-
-            if colors[i-1] != colors[i]:
-                dp[i] = dp[i-1] 
-            else:
-
-                dp[i] = dp[i-1] + min(neededTime[i-1], neededTime[i])
-                neededTime[i] = max(neededTime[i], neededTime[i - 1])
-            
-        return dp[-1]
-
-            
-            
