4e4b0635f0107d3024c72f4e02f5c68b9655244e
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-11-16 19:26:14 -0800

    Time: 1785 ms (5.09%), Space: 61.3 MB (97.09%) - LeetHub

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -0,0 +1,28 @@
+using namespace std;
+
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int apart = 0;
+        bool first_one = false;
+     for (int i = 0; i < nums.size(); i++){
+        cout << apart << endl;
+        cout << "Value of first_one" << first_one << endl;
+        if (nums[i] == 0 && first_one){
+            apart +=1;
+        }
+        else if (nums[i] == 1){
+            cout << "Entered this condition" <<endl;
+            if (!first_one){
+                first_one = true;
+                continue;
+            }
+            else if (apart < k){
+                return false;
+            }
+            apart = 0;
+        }
+     }   
+     return true;
+    }
+};
