c53ec0e3ee3bf90e23ab2bce572b000a51ab6880
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-11-16 19:26:38 -0800

    Time: 4 ms (10.91%), Space: 61.4 MB (74.91%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -6,13 +6,11 @@ public:
         int apart = 0;
         bool first_one = false;
      for (int i = 0; i < nums.size(); i++){
-        cout << apart << endl;
-        cout << "Value of first_one" << first_one << endl;
         if (nums[i] == 0 && first_one){
             apart +=1;
         }
         else if (nums[i] == 1){
-            cout << "Entered this condition" <<endl;
+
             if (!first_one){
                 first_one = true;
                 continue;
