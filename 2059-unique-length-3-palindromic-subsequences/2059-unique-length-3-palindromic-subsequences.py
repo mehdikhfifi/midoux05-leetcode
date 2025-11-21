26dd9553a2280d73e84ae8566c0a1be298f0c18a
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-11-20 18:31:49 -0800

    Time: 81 ms (94.37%), Space: 18.3 MB (39.64%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
@@ -1,30 +1,15 @@
 class Solution:
-    def countPalindromicSubsequence(self, s: str) -> int:
-        
+    def countPalindromicSubsequence(self, s: str) -> int:   
+        if len(s) <= 2:
+            return 0
 
-        fl = [[-1,-1] for _ in range(26)] 
-
-        for index, char in enumerate(s):
-            alpha = ord(char) - ord('a')
-            # print(alpha)
-            if fl[alpha][0] == -1:
-                fl[alpha][0] = index
-                fl[alpha][1] = index
-            elif fl[alpha][1] != -1:
-                fl[alpha][1] = index
-        # print(fl)
+        chars = set(s)
         res = 0
-        for alphabet in range(26):
-            if (fl[alphabet] == [-1,-1]) or (fl[alphabet][0] == fl[alphabet][1]):
-                continue
-            print(fl[alphabet])
-            temp_set = set()
-            for i in range(fl[alphabet][0]+1, fl[alphabet][1]):
-                temp_set.add(s[i])
-            res += len(temp_set)
-        
+        for ch in chars:
+            first = s.find(ch)
+            last = s.rfind(ch)
+
+            if first != last:
+                res += len(set(s[first + 1:last]))
+
         return res
-
-
-
-            
