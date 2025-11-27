bb8ebbd2806da4e73e3dffafc230a4f70548ccad
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-11-27 16:12:06 -0500

    Time: 699 ms (5.56%), Space: 42.8 MB (65.97%) - LeetHub

---
+++ 3381-maximum-subarray-sum-with-length-divisible-by-k/3381-maximum-subarray-sum-with-length-divisible-by-k.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maxSubarraySum(self, nums: List[int], k: int) -> int:
+        nums.insert(0,0)
+        for index,value in enumerate(nums):
+            if index == 0:
+                continue
+            nums[index] = nums[index] + nums[index-1]
+        
+        n = len(nums)
+        
+        res = -float('inf')
+
+
+        for i in range(k):
+            tmp_arr = []
+            for j in range(i,n-k,k):
+                tmp_arr.append(nums[j+k] - nums[j])
+            print(tmp_arr)
+
+            # run kadane's algo
+            running_sum = 0
+            for x in tmp_arr:
+                running_sum += x
+                print(running_sum)
+                res = max(running_sum, res)
+                if running_sum < 0:
+                    running_sum = 0
+        return res
+
+
+
+
+
+            
