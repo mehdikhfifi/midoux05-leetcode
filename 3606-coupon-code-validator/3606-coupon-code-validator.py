b10f9cb9cfe59014c14d2ee219af7edde6909cf7
midoux05 <145874140+mehdikhfifi@users.noreply.github.com>
2025-12-13 12:35:36 -0800

    Time: 7 ms (71.11%), Space: 18.2 MB (32.06%) - LeetHub

--- 3606-coupon-code-validator/3606-coupon-code-validator.py
+++ 3606-coupon-code-validator/3606-coupon-code-validator.py
@@ -1,10 +1,17 @@
 class Solution:
-    def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]:
+    def validateCoupons(
+        self, code: List[str], businessLine: List[str], isActive: List[bool]
+    ) -> List[str]:
         import re
 
         def is_alnum_underscore(s):
-            return bool(re.fullmatch(r'[A-Za-z0-9_]+', s))
-        res = [(code[i],i) for i in range(len(code)) if isActive[i] and code[i] != ""  and businessLine[i] != 'invalid']
+            return bool(re.fullmatch(r"[A-Za-z0-9_]+", s))
+
+        res = [
+            (code[i], i)
+            for i in range(len(code))
+            if isActive[i]  and businessLine[i] != "invalid"
+        ]
         # print(res)
-        res.sort(key = lambda x: (businessLine[x[1]], x[0]))
+        res.sort(key=lambda x: (businessLine[x[1]], x[0]))
         return [x[0] for x in res if is_alnum_underscore(x[0])]
